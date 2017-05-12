b33bb4afd4abecaa5a0ff450d1ab4ec2c9f6d728
xiaodaoM <295621649@qq.com>
2017-05-12 20:39:07 +0800

    使用curator 创建一个节点

--- zk/src/main/java/com/zk/curator/Create_Node_Sample.java
+++ zk/src/main/java/com/zk/curator/Create_Node_Sample.java
@@ -1,9 +1,35 @@
 package com.zk.curator;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.log4j.chainsaw.Main;
+import org.apache.zookeeper.CreateMode;
+
 /**
  * 使用 curator  创建节点
  * @author zqp
  *
  */
 public class Create_Node_Sample {
+	private static String path = "/zk-book/c1";
+	
+	 static CuratorFramework client =  CuratorFrameworkFactory.builder()
+			 .connectString("127.0.0.1:2181")
+			 .sessionTimeoutMs(30000)
+			 .connectionTimeoutMs(50000)
+			 .retryPolicy( new ExponentialBackoffRetry(1000, 3))
+			 .build();
+	 
+	 public static void main(String[] args) throws Exception {
+		
+		 client.start();
+		 
+		 client.create()
+		 .creatingParentsIfNeeded()
+		 .withMode(CreateMode.EPHEMERAL)
+		 .forPath(path,"init".getBytes());
+		 
+	}
 
 }
